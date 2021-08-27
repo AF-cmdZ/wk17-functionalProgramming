77c698e6f93fd3ddcd3a2660e8e771d8454e0071
A Fish <ashley.paz.fish@gmail.com>
2021-08-26 21:49:57 -0500

    add dev/designer hybrid

--- app/employees.js
+++ app/employees.js
@@ -38,8 +38,59 @@ const withEmail = (state) => ({
       ...withVaca(employee),
     };
   };
+
+  const withCode = (state) => ({
+    code(project) {
+      return `${state.name} is coding ${project} 👩🏾‍💻 in ${state.language}`;
+    },
+  });
   
-  const newEmp = createEmployee({ name: "Blanche", email: "blanche@gmail.com" });
-  const betterEmp = createEmployee({ name: "Rose", email: "rose@gmail.com" });
-  console.log(newEmp.getEmail(), newEmp.getPayCheck(), newEmp.giveRaise(1000));
+  const withDesign = (state) => ({
+    design(project) {
+        return `${state.name} is designing a ${state.media} ${project} 💻👩🏾 `;
+    },
+  });
+  
+  const createDeveloper = ({ name, email, language = "JS" }) => {
+    const state = {
+      language,
+    };
+  
+    return {
+      ...createEmployee({ name, email }),
+      ...withCode(state),
+    };
+  };
+  
+  const createDesigner = ({ name, email, media = "Print" }) => {
+    const state = {
+      media,
+    };
+  
+    return {
+      ...createEmployee({ name, email }),
+      ...withDesign(state),
+    };
+  };
+  
+  const createDesignerDeveloper = ({ name, email }) => ({
+    ...createDesigner({ name, email }),
+    ...createDeveloper({ name, email }),
+  });
+  
+const newEmp = createEmployee({ name: "Blanche", email: "blanche@gmail.com" });
+const betterEmp = createEmployee({ name: "Rose", email: "rose@gmail.com" });
+
+const dev = createDeveloper({ name: "JS Dev", email: "coder@gam.com" });
+
+const designer = createDesigner({ name: "Designer", email: "design@gam.com" });
+
+const dd = createDesignerDeveloper({
+  name: "Designer Dev",
+  email: "hbrid@gam.com",
+});
+
+console.log(dd);
+console.log(newEmp.getEmail(), newEmp.getPayCheck(), newEmp.giveRaise(1000));
+console.log(dev);
   
