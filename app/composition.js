c13217b268c1cc9d5b28add287729019825e3e39
A Fish <ashley.paz.fish@gmail.com>
2021-08-26 19:39:05 -0500

    composition

--- app/composition.js
+++ app/composition.js
@@ -1,31 +1,41 @@
 const withPic = (state) => ({
     takePicture() {
-      return `${state.name} took a picture!`;
+    return `${state.name} took a picture!`;
     },
-  });
-  
+});
+
 const withPhone = (state) => ({
 dial(number) {
     return `${state.name} is dialing the number: ${number}.`;
 },
 });
-  
+
 const withNoise = (state) => ({
     makeNoise() {
         return `We are making noise. ${state.noise} ${state.noise} ${state.noise}`;
-    },
-    });
+},
+});
 
-    const withDry = (state) => ({
-        dry() {
-            return `We are drying. Check back in ${state.dryingTime} minutes. ${state.noise}`;
-        },
-        });
+const withDry = (state) => ({
+    dry() {
+        return `We are drying. Check back in ${state.dryTime} minutes. ${state.noise}`;
+},
+});
 
-const createSmartAppliance = (name, noise, dryTime) => {
+const createSmartAppliance = (name, noise, dryTime = 100) => {
     const state = {
         name,
         noise,
         dryTime,
     };
+    return  {
+        ...withNoise(state),
+        ...withDry(state),
+        ...withPhone(state),
+        ...withPic(state),
+    };
 };
+
+const sd = createSmartAppliance("whirl", "Whirr", 5);
+
+console.log(sd, sd.takePicture(), sd.dial(12345), sd.makeNoise(), sd.dry());
